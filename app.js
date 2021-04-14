0b2d8aa7124c01225b84ed7d6426fa822f945ec9
Christian Obed Arana navas <COANelmejor@gmail.com>
2021-04-14 16:24:30 -0600

    Cambios en /app.js - Modificado ontencion de ruta home - Agregado middleware de reconocmiento de lenguaje del navegador - Agregada ruta de ventas

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/home');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -12,9 +12,20 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+  const language = req.acceptsLanguages('en', 'es', 'pr');
+  if (language) {
+    req.language = language;
+  } else {
+    req.language = 'es'
+  }
+  next();
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/ventas', require('./routes/ventas'));
 
 module.exports = app;
